7914cb2e73584ecf4cb947c1c60143cbf84ce344
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-26 20:03:56 +0200

    HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,13 @@ from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(e):
